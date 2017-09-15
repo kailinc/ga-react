edebd64136cb243f2b98a524d3760bf13c6acb1a
kailinc <kailinc@bu.edu>
2017-09-15 10:27:50 -0400

    Created Movie Component
    Comfort: 5
    Clarity: 5
    
    created movie component and tied it to index.js to render

---
+++ ga-self-study-react/lord-of-the-rings/src/Movie.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Movie extends Component {
+  render() {
+    return(
+      <div>
+        <h1>The Lord of the Rings: A Trilogy </h1>
+        <p> 4h 37 min </p>
+      </div>
+    )
+  }
+}
+
+export default Movie
