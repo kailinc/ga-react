26a8210bd1c152f55c5dec0ea36fcacb09f926c9
kailinc <kailinc@bu.edu>
2017-11-25 14:51:12 -0500

    Finished JSX Section
    Comfort: 5
    Clarity: 5
    
    JSX is very easy to understand. Getting bored.

--- react-web-dev-udemy-study/indecision-app/public/scripts/app.js
+++ react-web-dev-udemy-study/indecision-app/public/scripts/app.js
@@ -1,11 +1,41 @@
 'use strict';
 
 // JSX - JavaScript XML
-var template = React.createElement(
-  'p',
+var tempalte = React.createElement(
+  'div',
   null,
-  'This is pretty dope'
+  React.createElement(
+    'h1',
+    null,
+    'Indecision App'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'For people who dont know what to do with their life'
+  )
 );
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Kailin Chen'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: 22'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: Boston, MA'
+  )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- react-web-dev-udemy-study/indecision-app/src/app.js
+++ react-web-dev-udemy-study/indecision-app/src/app.js
@@ -1,8 +1,22 @@
 // JSX - JavaScript XML
-var template = <p>This is pretty dope</p>;
+var tempalte = (
+  <div>
+    <h1>Indecision App</h1>
+    <p>For people who dont know what to do with their life</p>
+  </div>
+)
+
+var templateTwo = (
+  <div>
+    <h1>Kailin Chen</h1>
+    <p>Age: 22</p>
+    <p>Location: Boston, MA</p>
+  </div>
+)
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(
-  template,
+  templateTwo,
   appRoot
 );
