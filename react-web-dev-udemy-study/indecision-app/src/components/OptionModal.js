35d0b7b3b32f4bc5fdb32ffd9a8208e1a3187a8f
kailinc <kailinc@bu.edu>
2017-12-15 10:35:46 +0700

    Refactored Code
    use shorthand the implicit return JSX instead of using explicit return

--- react-web-dev-udemy-study/indecision-app/src/components/OptionModal.js
+++ react-web-dev-udemy-study/indecision-app/src/components/OptionModal.js
@@ -2,16 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
 
-const OptionModal = (props) => {
-  return(
+const OptionModal = (props) => (
     <Modal
       isOpen={ !!props.selectedOption }
       contentLabel="Selected Option"
+      onRequestClose={ props.handleClick }
       >
       <h3>{ props.selectedOption }</h3>
       <button onClick={ props.handleClick }>Close</button>
     </Modal>
-  )
-}
+)
 
 export default OptionModal;
