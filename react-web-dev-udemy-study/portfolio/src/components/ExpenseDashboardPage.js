d7707d0dfb6ea67e6afc8259843daa88cb6db064
kailinc <kailinc@bu.edu>
2017-12-16 16:11:43 +0700

    Initialized Portfolio Boilerplate
    - duplicated expensify application
    - removed unnessary code, files, folders

---
+++ react-web-dev-udemy-study/portfolio/src/components/ExpenseDashboardPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const ExpenseDashboardPage = (props) => {
+  return (
+    <div>
+      Expense Dash Board
+    </div>
+  )
+}
+
+export default ExpenseDashboardPage;
---
+++ react-web-dev-udemy-study/portfolio/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {NavLink} from 'react-router-dom';
+
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+      <nav>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Dash Board</NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+        <NavLink to="/help" activeClassName="is-active">Help Page</NavLink>
+      </nav>
+  </header>
+)
+
+
+export default Header;
